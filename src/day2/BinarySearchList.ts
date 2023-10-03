a2da6599268a6beae2a7bf55af25ecb3f5361baa
sampiecz <sam@samuelpiecz.com>
2023-10-04 00:11:38 +0200

    feat: this is a fork of primeagen's ts algorithms course on frontend masters, I'm re-working it as I don't like the way it's structured config wise, otherwise, list of algorithms I'm going through & tests etc should be the same

---
+++ src/day2/BinarySearchList.ts
@@ -0,0 +1,23 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+  // Assume array is sorted
+
+  let lo = 0;
+  let hi = haystack.length;
+
+  do {
+
+    const m = Math.floor(lo + (hi - lo) / 2);
+    const v = haystack[m];
+
+    if (v === needle) {
+      return true;
+    } else if (v > needle) {
+      hi = m;
+    } else {
+      lo = m + 1;
+    }
+  } while (lo < hi);
+
+  return false;
+
+}
